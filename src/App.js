a0ec165bf819e74bfb186d57e77168f64e50d1b6
mattyydagz <mnjdagostino@gamil.com>
2021-08-19 12:42:53 -0400

    Add new tasks & close/open from

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 // import React from "react"; for class example
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 import { useState } from "react";
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false);
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -18,6 +20,13 @@ function App() {
       reminder: false,
     },
   ]);
+
+  //Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1;
+    const newTask = { id, ...task };
+    setTasks([...tasks, newTask]);
+  };
 
   // Delete Task
   const deleteTask = (id) => {
@@ -35,7 +44,11 @@ function App() {
 
   return (
     <div className="container">
-      <Header />
+      <Header
+        onAdd={() => setShowAddTask(!showAddTask)}
+        showAdd={showAddTask}
+      />
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
