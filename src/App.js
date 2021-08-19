978a95341a814f1659708e554f737aaee3b6483f
mattyydagz <mnjdagostino@gamil.com>
2021-08-19 12:12:24 -0400

    Added delete button and func and, reminder func

--- src/App.js
+++ src/App.js
@@ -1,10 +1,46 @@
 // import React from "react"; for class example
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import { useState } from "react";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Learn React",
+      day: "Aug 19th at 11:00am",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Make lunch",
+      day: "Aug 19th at 12:00am",
+      reminder: false,
+    },
+  ]);
+
+  // Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  // Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
+
   return (
     <div className="container">
       <Header />
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        "You currently have no tasks."
+      )}
     </div>
   );
 }
